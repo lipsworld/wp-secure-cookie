00f605d664cbdef6c1d62b16a1b0fd4b16ba2d5b
Andrew Grady <agrady42@gmail.com>
2014-06-07 13:38:43 -0400

    Complete. Updated readme

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-secure-cookie
+Secure Cookie
 =============
 
-Wordpress - marks wordpress_logged_in as Secure 
+A WordPress plugin that marks wordpress_logged_in as Secure if using https.
+
+Written for WordPress 3.9.1 & php 5.5.9, not tested for backwards compatibility.
--- secure-cookie.php
+++ secure-cookie.php
@@ -6,9 +6,18 @@
 /*
 Plugin Name: Secure Cookie
 Plugin URI: https://github.com/agrady42/secure-cookie
-Description: Marks wordpress_logged_in cookie as Secure
+Description: Marks wordpress_logged_in cookie as Secure if using https
 Auther: Andrew Grady
 Version: 1.0
 Author URI: https://github.com/agrady42
 Licence: MIT
  */
+
+function secure_cookie($cookie, $user_id, $secure) {
+	if (is_ssl()) {
+		return true;
+	}
+	return false;
+}
+
+add_filter('secure_logged_in_cookie', 'secure_cookie', 10, 3);
