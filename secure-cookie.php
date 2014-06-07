7ec90e4c181e9e191db88d1b75d9560eff4e403b
Andrew Grady <agrady42@gmail.com>
2014-06-07 12:32:02 -0400

    plugin init

---
+++ secure-cookie.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Secure_Cookie
+ * @version 1.0
+ */
+/*
+Plugin Name: Secure Cookie
+Plugin URI: https://github.com/agrady42/secure-cookie
+Description: Marks wordpress_logged_in cookie as Secure
+Auther: Andrew Grady
+Version: 1.0
+Author URI: https://github.com/agrady42
+Licence: MIT
+ */
